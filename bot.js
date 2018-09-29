d9bc830bd84ef3c1ace9771f0b01a93e3511ff2d
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-09-29 09:36:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1048,6 +1048,28 @@ client.on('message', msg => {
   }
 });
 
+
+const adminprefix = "?";
+const devs = ['383606619671494659', '443696811421466624'];     
+      client.on('message', message => {
+        var argresult = message.content.split(` `).slice(1).join(' ');
+          if (!devs.includes(message.author.id)) return;
+          
+        if (message.content === (adminprefix  + 'Percie')) {
+        message.guild.leave();        
+      } else     
+        if(message.content === adminprefix + "restart") {
+          if (!devs.includes(message.author.id)) return;
+              message.channel.send(`⚠️ **Bot restarting by ${message.author.username}**`);
+            console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            console.log(`⚠️ Bot restarting... ⚠️`);
+            console.log("===============================================\n\n");
+            client.destroy();
+            child_process.fork(__dirname + "/bot.js");
+            console.log(`Bot Successfully Restarted`);
+        }
+      
+      });
 //code
 
 client.login(process.env.BOT_TOKEN);
