8c5fc0e349107f6b067139b437f6dd7f1398c709
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-09-29 09:18:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,60 +57,10 @@ client.user.setStatus("online")
 });
 
 
-client.on('message', message => {
-var args = message.content.split(" ").slice(1);    
-if(message.content.startsWith(prefix + 'id')) {
-var year = message.author.createdAt.getFullYear()
-var month = message.author.createdAt.getMonth()
-var day = message.author.createdAt.getDate()
-var men = message.mentions.users.first();  
-let args = message.content.split(' ').slice(1).join(' ');
-if (args == '') {
-var z = message.author;
-}else {
-var z = message.mentions.users.first();
-}
 
-let d = z.createdAt;          
-let n = d.toLocaleString();   
-let x;                       
-let y;                        
-
-if (z.presence.game !== null) {
-y = `${z.presence.game.name}`;
-} else {
-y = "No Playing... |💤.";
-}
-if (z.bot) {
-var w = 'بوت';
-}else {
-var w = 'عضو';
-}
-let embed = new Discord.RichEmbed()
-.setColor("#502faf")
-.addField('🔱| اسمك:',`**<@` + `${z.id}` + `>**`, true)
-.addField('🛡| ايدي:', "**"+ `${z.id}` +"**",true)
-.addField('♨| Playing:','**'+y+'**' , true)
-.addField('🤖| نوع حسابك:',"**"+ w + "**",true)    
-.addField('📛| الكود حق حسابك:',"**#" +  `${z.discriminator}**`,true)
-.addField('**التاريح الذي انشئ فيه حسابك | 📆 **: ' ,year + "-"+ month +"-"+ day)    
-.addField("**تاريخ دخولك للسيرفر| ⌚   :**", message.member.joinedAt.toLocaleString())    
-
-.addField('**⌚ | تاريخ انشاء حسابك الكامل:**', message.author.createdAt.toLocaleString())
-.addField("**اخر رسالة لك | 💬  :**", message.author.lastMessage)            
-
-.setThumbnail(`${z.avatarURL}`)
-.setFooter(message.author.username, message.author.avatarURL)
-
-message.channel.send({embed});
-  if (!message) return message.reply('**ضع المينشان بشكل صحيح  ❌ **').catch(console.error);
-
-}
-
-});
 
 client.on('message', message => {
-    if (message.content === "?serv") {
+    if (message.content === "?id") {
     let embed = new Discord.RichEmbed()
    .setColor("RANDOM")
    .setThumbnail(message.author.avatarURL)
@@ -272,7 +222,7 @@ client.on('message', message => {
    });
 client.on('message', function(msg) {
     
-    if(msg.content.startsWith (prefix  + 'serv2')) {
+    if(msg.content.startsWith (prefix  + 'serv')) {
       let embed = new Discord.RichEmbed()
       .setColor('RANDOM')
       .setThumbnail(msg.guild.iconURL)
@@ -324,7 +274,6 @@ if (message.content.startsWith(prefix + 'help')) { //ALPHACODES
         ***__General orders__***
 **
 『?serv /يعرض لك معلومات عن السيرفر』
-『?serv2 / يعرض لك معلومات عن السيرفر ( الكود الثاني ) للمعلومات』
 『?id / يعرض لك معلومات عنك』
 『?myroles / لرؤية جميع رتبك الشخصية بالسيرفر』
 『?helpadmin / مساعدة الادمن』
