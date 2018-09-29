f28b142626080d459fdb2070f0687c34abdb2e0a
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-09-29 14:32:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,35 +58,60 @@ client.user.setStatus("online")
 
 
 
-
 client.on('message', message => {
-    if (message.content === "?id") {
-    let embed = new Discord.RichEmbed()
-   .setColor("RANDOM")
-   .setThumbnail(message.author.avatarURL)
-   .setTitle(`info about ${message.guild.name}`)
-   .addField("Server Owner 👑",`➥ ` + `${message.guild.owner.user.username}`, true)
-   .addField('Server ID 🆔',`➥` + message.guild.id, true)
-   .addField("Owner Tag",`➥ ` +  `#` + message.guild.owner.user.discriminator, true)
-   .addField("Owner ID 🆔",`➥ ` + message.guild.owner.user.id, true)
-   .addField("Server Region📡",`➥ ` + message.guild.region, true)
-   .addField("Server Member Size🏧",`➥ ` + message.guild.members.size, true)
-   .addField("Server Channels Number🏧",`➥ ` + message.guild.channels.size, true)
-   .addField("Server Roels Number🏧",`➥ ` + message.guild.roles.size, true)
-   .addField("AFK channel💤",`➥ ` + message.guild.afkChannel || 'Null', true)
-   .addField("Server Created AT",`➥ ` + message.guild.createdAt, true)
-   .addField(`info about ${message.author.username}`, `➥ `)
-   .addField("Name",`➥ ` + `${message.author.username}`, true)
-   .addField('Tag',`➥ ` + "#" +  message.author.discriminator, true)
-   .addField("ID 🆔",`➥ ` + message.author.id, true)
-   .addField(" Account Created At",`➥ ` + message.author.createdAt, true)
-   .setTimestamp()
-   .setFooter(message.author.tag, message.author.avatarURL)
-      
-      
-   message.channel.sendEmbed(embed);
-     }
- });
+  var prefix = "?"
+var args = message.content.split(" ").slice(1);    
+if(message.content.startsWith(prefix + 'id')) {
+var year = message.author.createdAt.getFullYear()
+var month = message.author.createdAt.getMonth()
+var day = message.author.createdAt.getDate()
+var men = message.mentions.users.first();  
+let args = message.content.split(' ').slice(1).join(' ');
+if (args == '') {
+var z = message.author;
+}else {
+var z = message.mentions.users.first();
+}
+
+let d = z.createdAt;          
+let n = d.toLocaleString();   
+let x;                       
+let y;                        
+
+if (z.presence.game !== null) {
+y = `${z.presence.game.name}`;
+} else {
+y = "No Playing... |💤.";
+}
+if (z.bot) {
+var w = 'بوت';
+}else {
+var w = 'عضو';
+}
+let embed = new Discord.RichEmbed()
+.setColor("#502faf")
+.addField('🔱| اسمك:',`**<@` + `${z.id}` + `>**`, true)
+.addField('🛡| ايدي:', "**"+ `${z.id}` +"**",true)
+.addField('♨| Playing:','**'+y+'**' , true)
+.addField('🤖| نوع حسابك:',"**"+ w + "**",true)    
+.addField('📛| الكود حق حسابك:',"**#" +  `${z.discriminator}**`,true)
+.addField('**التاريح الذي انشئ فيه حسابك | 📆 **: ' ,year + "-"+ month +"-"+ day)    
+.addField("**تاريخ دخولك للسيرفر| ⌚   :**", message.member.joinedAt.toLocaleString())    
+
+.addField('**⌚ | تاريخ انشاء حسابك الكامل:**', message.author.createdAt.toLocaleString())
+.addField("**اخر رسالة لك | 💬  :**", message.author.lastMessage)            
+
+.setThumbnail(`${z.avatarURL}`)
+.setFooter(message.author.username, message.author.avatarURL)
+
+message.channel.send({embed});
+  if (!message) return message.reply('**ضع المينشان بشكل صحيح  ❌ **').catch(console.error);
+
+}
+
+});
+
+
 client.on('message', message=>{
  // البريفكس حقك هنا 
 
