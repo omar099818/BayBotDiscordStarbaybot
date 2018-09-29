0c45debefc3326b36557ba4a365bad72435db4ae
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-09-29 13:38:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1104,6 +1104,23 @@ client.on("message", message => {
 	} 
 });
 
+client.on('message', ra3d => {
+var prefix = "-";
+                        let args = ra3d.content.split(" ").slice(1).join(" ")
+if(ra3d.content.startsWith(prefix + 'ccolors')) {
+    if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
+             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
+              ra3d.channel.send(`**✅ |Created __${args}__ Colors**`);
+                  setInterval(function(){})
+                    let count = 0;
+                    let ecount = 0;
+          for(let x = 1; x < `${parseInt(args)+1}`; x++){
+            ra3d.guild.createRole({name:x,
+              color: 'RANDOM'})
+              }
+            }
+       });
+
 //code
 
 client.login(process.env.BOT_TOKEN);
